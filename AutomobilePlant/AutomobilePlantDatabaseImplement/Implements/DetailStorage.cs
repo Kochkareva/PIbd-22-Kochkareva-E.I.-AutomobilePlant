f130b21d23cd80d02429fa0f6c69383f6f4f4f0c
Елизавета Кочкарева <lissakim.1821@gmail.com>
2022-03-12 16:06:35 +0400

    Laboratornaya 3 done

---
+++ AutomobilePlant/AutomobilePlantDatabaseImplement/Implements/DetailStorage.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomobilePlantContracts.BindingModels;
+using AutomobilePlantContracts.StoragesContracts;
+using AutomobilePlantContracts.ViewModels;
+using AutomobilePlantDatabaseImplement.Models;
+
+namespace AutomobilePlantDatabaseImplement.Implements
+{
+    public class DetailStorage : IDetailStorage
+    {
+        public List<DetailViewModel> GetFullList()
+        {
+            using var context = new AutomobilePlantDatabase();
+            return context.Details
+            .Select(CreateModel)
+            .ToList();
+        }
+        public List<DetailViewModel> GetFilteredList(DetailBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            using var context = new AutomobilePlantDatabase();
+            return context.Details
+            .Where(rec => rec.DetailName.Contains(model.DetailName))
+            .Select(CreateModel)
+            .ToList();
+        }
+        public DetailViewModel GetElement(DetailBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            using var context = new AutomobilePlantDatabase();
+            var component = context.Details
+            .FirstOrDefault(rec => rec.DetailName == model.DetailName || rec.Id
+           == model.Id);
+            return component != null ? CreateModel(component) : null;
+        }
+        public void Insert(DetailBindingModel model)
+        {
+            using var context = new AutomobilePlantDatabase();
+            context.Details.Add(CreateModel(model, new Detail()));
+            context.SaveChanges();
+        }
+        public void Update(DetailBindingModel model)
+        {
+            using var context = new AutomobilePlantDatabase();
+            var element = context.Details.FirstOrDefault(rec => rec.Id == model.Id);
+            if (element == null)
+            {
+                throw new Exception("Элемент не найден");
+            }
+            CreateModel(model, element);
+            context.SaveChanges();
+        }
+        public void Delete(DetailBindingModel model)
+        {
+            using var context = new AutomobilePlantDatabase();
+            Detail element = context.Details.FirstOrDefault(rec => rec.Id == model.Id);
+            if (element != null)
+            {
+                context.Details.Remove(element);
+                context.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Элемент не найден");
+            }
+        }
+        private static Detail CreateModel(DetailBindingModel model, Detail detail)
+        {
+            detail.DetailName = model.DetailName;
+            return detail;
+        }
+        private static DetailViewModel CreateModel(Detail component)
+        {
+            return new DetailViewModel
+            {
+                Id = component.Id,
+                DetailName = component.DetailName
+            };
+        }
+    }
+}
---
+++ AutomobilePlant/AutomobilePlantDatabaseImplement/Models/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutomobilePlantDatabaseImplement.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        [Required]
+        public string CarName { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+
+        [ForeignKey("CarId")]
+        public virtual List<CarDetail> CarDetails { get; set; }
+
+        [ForeignKey("CarId")]
+        public virtual List<Order> Order { get; set; }
+    }
+}
