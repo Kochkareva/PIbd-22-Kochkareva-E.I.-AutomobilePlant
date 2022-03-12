4edbc342cd652f1ee556f1c87a696ac93ed6be0b
Елизавета Кочкарева <lissakim.1821@gmail.com>
2022-03-12 12:01:59 +0400

    Laboratornaya 2 done

---
+++ AutomobilePlant/AutomobilePlantFileImplement/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobilePlantFileImplement.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public string CarName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Dictionary<int, int> CarDetails { get; set; }
+    }
+}
