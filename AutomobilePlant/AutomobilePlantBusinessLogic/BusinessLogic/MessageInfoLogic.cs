7fd487f3678b3315dc823d99b3d0b647c39961a5
Елизавета Кочкарева <lissakim.1821@gmail.com>
2022-06-09 22:03:04 +0400

    Разрешение конфликтов слияния

---
+++ AutomobilePlant/AutomobilePlantBusinessLogic/BusinessLogic/MessageInfoLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using AutomobilePlantContracts.BindingModels;
+using AutomobilePlantContracts.BusinessLogicsContracts;
+using AutomobilePlantContracts.StoragesContracts;
+using AutomobilePlantContracts.ViewModels;
+
+
+namespace AutomobilePlantBusinessLogic.BusinessLogics
+{
+    public class MessageInfoLogic : IMessageInfoLogic
+    {
+        private readonly IMessageInfoStorage _messageInfoStorage;
+        public MessageInfoLogic(IMessageInfoStorage messageInfoStorage)
+        {
+            _messageInfoStorage = messageInfoStorage;
+        }
+        public List<MessageInfoViewModel> Read(MessageInfoBindingModel model)
+        {
+            if (model == null)
+            {
+                return _messageInfoStorage.GetFullList();
+            }
+            return _messageInfoStorage.GetFilteredList(model);
+        }
+        public void CreateOrUpdate(MessageInfoBindingModel model)
+        {
+            _messageInfoStorage.Insert(model);
+        }
+    }
+}
